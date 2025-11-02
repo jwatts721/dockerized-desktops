e1f91a9460908fea8189b861cfd06f98feb683d2
Jamal Watts <jwatts721@gmail.com>
2025-11-02 16:37:01 -0500

    Initial Commit.

---
+++ browser-shell/start-stream.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Start virtual X server
+Xvfb :0 -screen 0 1920x1080x24 &
+export DISPLAY=:0
+
+# Start window manager
+openbox &
+
+# Launch Chromium
+chromium \
+  --no-sandbox \
+  --disable-gpu \
+  --start-fullscreen \
+  --noerrdialogs \
+  --disable-infobars \
+  --disable-session-crashed-bubble \
+  --disable-features=TranslateUI &
+
+# Start x11vnc to stream the X session
+x11vnc -display :0 -nopw -forever -shared -bg
+
+# Start noVNC on port 8080
+/opt/websockify/run --web /opt/novnc 8080 localhost:5900
